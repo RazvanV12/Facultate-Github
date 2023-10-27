15b1e54eb565b97af656c90a267c2ee2f64d18b1
RazvanV12 <92295676+RazvanV12@users.noreply.github.com>
2023-10-27 07:14:59 +0300

    Unreal_Lab3

--- Proiecte POO/Proiect1/main.cpp
+++ Proiecte POO/Proiect1/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << std::endl;
-    return 0;
+void reverseWords(string &sentence){
+    int n = sentence.size(), j, stg, dr;
+    char aux;
+    for(int i = 0; i < n; i++){
+        if(sentence[i] != ' ')
+        {
+            j = i;
+            while(sentence[j] != ' ' && j < sentence.size()){
+                j++;
+            }
+            stg = i;
+            dr = j - 1;
+            while(stg < dr){
+                aux = sentence[stg];
+                sentence[stg] = sentence[dr];
+                sentence[dr] = aux;
+                stg++;
+                dr--;
+            }
+            i = j;
+        }
+    }
+}
+
+void reverseSentence(string &sentence){
+    int n sentence.size();
+
+}
+
+int main(){
+    string sentence = "   Acesta   este un    test";
+    reverseWords(sentence);
+    cout << sentence;
 }
