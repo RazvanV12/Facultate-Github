472dbfe075e58669a04e01183bd3213dbe705d4e
Velciu A Razvan-Gabriel <92295676+RazvanV12@users.noreply.github.com>
2024-03-17 03:59:12 +0200

    Update Program.cs

--- LeetCode_Problems/LeetCode_Problems/Program.cs
+++ LeetCode_Problems/LeetCode_Problems/Program.cs
@@ -781,13 +781,250 @@ namespace LeetCode_Problems
         */
         
         
+        // 128. Longest subsequence consecutive
+        // The idea is to transform the input array into a set so that we can have the lookup complexity of O(1). Then, for each element we check if it is the
+        // beginning of a sequence by checking if we have (n-1) in the set. If it is, then we check the length of the sequence by checking if (n+1) belongs
+        // to the set, then (n+2) and so on. We do that for every number that is the beginning of a sequence and so we will have the longest subsequence.
+        // The time complexity of the problem will be O(n).
+        public static int LongestConsecutive(int[] nums)
+        {
+            int longestSubsequence = 0;
+            HashSet<int> mySet = new HashSet<int>(nums);
+            foreach (var element in mySet)
+            {
+                // Check to see if it is the beginning of the sequence
+                if (!mySet.Contains(element - 1))
+                {
+                    int lengthCurrentSequence = 1;
+                    while (mySet.Contains(element + lengthCurrentSequence))
+                    {
+                        lengthCurrentSequence++;
+                    }
+
+                    if (lengthCurrentSequence > longestSubsequence)
+                        longestSubsequence = lengthCurrentSequence;
+                }
+            }
+            return longestSubsequence;
+        }
+        
+        // 56. Merge intervals 
+        // The first idea that comes to mind is to check for each interval all the intervals that have the start smaller than the end of the current one. If we find one
+        // we merge them, eliminate the interval from the matrix and we start looking again from the beginning.
+        public static int[][] Merge(int[][] intervals)
+        {
+            int size = intervals.Length;
+            int[][] result = new int[][] { };
+            for (int i = 0; i < size; i++)
+            {
+                while (true)
+                {
+                    bool merged = false;
+                    for (int j = i + 1; j < size; j++)
+                        if (intervals[i][1] >= intervals[j][0] && intervals[i][0] <= intervals[j][1])
+                        {
+                            if (intervals[i][0] >= intervals[j][0] && intervals[i][1] <= intervals[j][1])
+                            {
+                                intervals[i][0] = intervals[j][0];
+                                intervals[i][1] = intervals[j][1];
+                            }
+
+                            if (intervals[i][0] <= intervals[j][0] && intervals[i][1] <= intervals[j][1])
+                            {
+                                intervals[i][1] = intervals[j][1];
+                            }
+
+                            if (intervals[i][0] >= intervals[j][0] && intervals[i][1] >= intervals[j][1])
+                            {
+                                intervals[i][0] = intervals[j][0];
+                            }
+                            
+
+                            for (int k = j; k < size - 1; k++)
+                            {
+                                intervals[k][0] = intervals[k + 1][0];
+                                intervals[k][1] = intervals[k + 1][1];
+                            }
+                            size--;
+                            merged = true;
+                            break;
+                        }
+
+                    if (!merged)
+                        break;
+                }
+            }
+            result = new int[size][];
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = new int[] { intervals[i][0], intervals[i][1] };
+            }
+            return result;
+        }
+        
+        // 57. Insert Interval
+        // The idea is to first decide where the newInterval should be. We do that by looking at it's start and it's position will be between 2 consecutive intervals 
+        // such that the start of newInterval is smaller than start of rightInterval and bigger than start of LeftInterval. After that, we simply check if there is an
+        // overlap, if so, we merge the necessary intervals.
+
+        // public static int[][] Insert(int[][] intervals, int[] newInterval)
+        // {
+        //     // first we find the position where we have to insert the newInterval
+        //     int positionToInsert = 0;
+        //     for (int i = 0; i < intervals.Length; i++)
+        //     {
+        //         if (intervals[i][0] > newInterval[0])
+        //             positionToInsert = i;
+        //     }
+        //     // Special cases: If intervals is empty or if we have to insert it at the beginning of the array
+        //     if (intervals.Length == 0)
+        //     {
+        //         int[][] auxResult = new int[][] { newInterval };
+        //         return auxResult;
+        //     }
+        //     // We will first check to see how many intervals we have to merge in order to keep the array with non-overlapping intervals
+        //     int mergeCounter = 0;
+        //     if (positionToInsert != 0)
+        //     {
+        //         if (intervals[positionToInsert - 1][1] >= newInterval[0])
+        //             mergeCounter++;
+        //     }
+        //
+        //     for (var i = positionToInsert; i < intervals.Length; i++)
+        //     {
+        //         if (newInterval[1] >= intervals[i][0])
+        //             mergeCounter++;
+        //     }
+        //
+        //     int[][] result = new int[intervals.Length - mergeCounter][];
+        //     // If it is possible to merge with the interval that's on the left, then we have to copy all the intervals until positionToInsert - 2, then we add
+        //     // the merged interval and then the rest.
+        //     if (intervals[positionToInsert - 1][1] >= newInterval[0])
+        //     {
+        //         for (var i = 0; i < positionToInsert - 1; i++)
+        //         {
+        //             result[i][0] = intervals[i][0];
+        //             result[i][1] = intervals[i][1];
+        //         }
+        //
+        //         int[] mergedInterval = new int[2];
+        //         mergedInterval[0] = intervals[positionToInsert - 1][0];
+        //         mergedInterval[1] = intervals[positionToInsert + mergeCounter - 1][1];
+        //         result[positionToInsert][0] = mergedInterval[0];
+        //         result[positionToInsert][1] = mergedInterval[1];
+        //         for (var i = positionToInsert + 1; i < intervals.Length; i++)
+        //         {
+        //             result[i][0] = intervals[i + mergeCounter + 1][0];
+        //             result[i][1] = intervals[i + mergeCounter + 1][1];
+        //         }
+        //     }
+        //     else
+        //     {
+        //         for (var i = 0; i < positionToInsert; i++)
+        //         {
+        //             result[i][0] = intervals[i][0];
+        //             result[i][1] = intervals[i][1];
+        //         }
+        //
+        //         int[] mergedInterval = new int[2];
+        //         mergedInterval[0] = newInterval[0];
+        //         
+        //     }
+        // }
+        
+        // 57. Insert Interval
+        public static int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            List<int[]> result = new List<int[]>();
+            int i = 0;
+            // Add all the intervals that are before the newInterval
+            while (i < intervals.Length && intervals[i][1] < newInterval[0])
+            {
+                result.Add(intervals[i]);
+                i++;
+            }
+            // Merge all the intervals that overlap with the newInterval
+            while (i < intervals.Length && intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+                i++;
+            }
+            result.Add(newInterval);
+            // Add all the intervals that are after the newInterval
+            while (i < intervals.Length)
+            {
+                result.Add(intervals[i]);
+                i++;
+            }
+            return result.ToArray();
+        }
+        
+        // 172. Factorial Trailing Zeroes
+        // The first idea is to simply calculate n! and then check how many trailing zeroes it has. The time complexity will be O(n)
+
+        public static int calculateFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return calculateFactorial(n - 1) * n;
+            }
+        }
+        // This is the solution for time complexity O(n)
+        public static int TrailingZeroes(int n)
+        {
+            int factorial = calculateFactorial(n);
+            int result = 0;
+            if (factorial == 0)
+                return 1;
+            while (factorial % 10 == 0)
+            {
+                result++;
+                factorial = factorial / 10;
+            }
+
+            return result;
+        }
+        
+        // Solution for the time complexity O(logn) is to understand that we get a trailing zero for each number multiple of 5 smaller than n. This happens because 
+        // we need a 10 to have a trailing zero, which is obtained from 5 * 2 , but the number of numbers multiple of 2 will always be greater than the number of 
+        // numbers multiple of 5. 
+        public static int TrailingZeroes2(int n)
+        {
+            int count = 0;
+
+            while (n >= 5) {
+                n /= 5;
+                count += n;
+            }
+            return count;
+        }
+        
+        // 50. Pow(x, n) 
+        public static int MyPow(int x, int n)
+        {
+            if (n == 0)
+                return 1;
+            if (n == 1)
+                return x;
+            if (n == -1)
+                return 1 / x;
+            if (n % 2 == 0)
+                return MyPow(x * x, n / 2);
+            else
+                return x * MyPow(x * x, n / 2);
+        }
         
         
         
         public static void Main(string[] args)
         {
-            string[] parameter = new string[] { "hhhhu","tttti","tttit","hhhuh","hhuhh","tittt" };
-            GroupAnagrams(parameter);
+            // Print factorial of 5 using the functon calculateFactorial
+            Console.WriteLine(TrailingZeroes2(9));
         }
     }
 }
